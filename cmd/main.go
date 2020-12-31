dba23481d40612a63fefef0b5ed9d0130e287236
Alexey Vasyukov <a.vasyukov1@yandex.ru>
2020-12-31 12:48:30 +0300

    Add configuration file & init viper

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/architectv/merchantx-task/pkg/handler"
 	"github.com/architectv/merchantx-task/pkg/repository"
 	"github.com/architectv/merchantx-task/pkg/service"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
@@ -16,5 +22,11 @@ func main() {
 	app := fiber.New()
 	app.Use(logger.New())
 	handlers.InitRoutes(app)
-	app.Listen(":8001")
+	app.Listen(viper.GetString("port"))
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,5 @@ go 1.15
 
 require (
 	github.com/gofiber/fiber/v2 v2.3.2
+	github.com/spf13/viper v1.7.1
 )
